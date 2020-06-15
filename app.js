c53c707284b074ef9b142fcd383d889c1c5132b6
marcCodez <marc_pulumbarit@hotmail.com>
2020-06-15 14:34:18 +1000

    added read me

---
+++ app.js
@@ -0,0 +1,90 @@
+const PageState = function() {
+    let currentState = new homeState(this);
+
+    this.init = function() {
+        // this.chane will change the current state
+        this.change(new homeState);
+    }
+
+    this.change = function(state) {
+        currentState = state;
+    }
+};
+
+// Home State
+const homeState = function(page){
+    // modify the UI depending on the state we are in
+    document.querySelector('#heading').textContent = null;
+    document.querySelector('#content').innerHTML = `
+    <div class="jumbotron">
+    <h1 class="display-3">Hello, world!</h1>
+    <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+    <hr class="my-4">
+    <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+    </p>
+  </div>
+    `;
+
+};
+
+// About state
+const aboutState = function(page) {
+    document.querySelector('#heading').textContent = 'About Us';
+    document.querySelector('#content').innerHTML = `
+        <p>This is the about page</p>
+    `;
+};
+
+// Contact state
+const contactState = function(page) {
+    document.querySelector('#heading').textContent = 'Contact Us';
+    document.querySelector('#content').innerHTML = `
+    <form>
+     <div class="form-group">
+      <label>Name</label>
+      <input type="text" class="form-control">
+    </div>
+    <div class="form-group">
+    <label>Email address</label>
+    <input type="email" class="form-control">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+    `;
+};
+
+// Instantiate pageState
+const page = new PageState();
+
+// Init the first state, home state by default 
+page.init();
+
+// Clicking the links will change the state
+// UI Vars
+const home = document.querySelector('#home')
+      about = document.querySelector('#about')
+      contact = document.querySelector('#contact')
+
+// Event listeners
+// Home
+home.addEventListener('click', (e) => {
+    page.change(new homeState);
+
+    e.preventDefault();
+});
+
+// About
+about.addEventListener('click', (e) => {
+    page.change(new aboutState);
+
+    e.preventDefault();
+});
+
+// Contact
+contact.addEventListener('click', (e) => {
+    page.change(new contactState);
+
+    e.preventDefault();
+});
